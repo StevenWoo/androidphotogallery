6cb90fd75ac6ed57cb61f2e5a31bd04f04979350
Steven Woo <swoo@tackable.com>
2014-05-11 18:53:36 -0700

    chapter28

--- PhotoGallery/src/main/java/com/tackable/foobar/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/main/java/com/tackable/foobar/android/photogallery/FlickrFetchr.java
@@ -26,15 +26,19 @@ public class FlickrFetchr {
     private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "0073e3ffe50f16aeece2b0e16ae03601";
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String METHOD_SEARCH = "flickr.photos.search";
     private static final String PARAM_EXTRAS = "extras";
     private static final String PARAM_FORMAT = "format";
     private static final String PARAM_JSON ="json";
     private static final String TAG = "FlickrFetchr";
     private static final String EXTRA_SMALL_URL = "url_s";
+    private static final String PARAM_TEXT = "text";
 
     private static final String JSON_ID = "id";
     private static final String JSON_CAPTION = "title";
     private static final String JSON_URL = "url_s";
+
+    public static final String PREF_SEARCH_QUERY = "searchQuery";
 
     private static final String XML_PHOTO = "photo";
 
@@ -66,58 +70,88 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public ArrayList<GalleryItem> fetchItems(){
+
+    public JSONArray extractPhotos(String inputString) {
+        JSONArray arrayJSON = null;
+        if( inputString != null ) {
+            try {
+                // need to remove jsonFlickrApi( from start
+                // need to remove ) from end for some crappy reason flickr put that in there
+                String start = "jsonFlickrApi(";
+                String end = ")";
+                if (inputString.length() > start.length() + end.length()) {
+                    inputString = inputString.substring(start.length(), inputString.length() - end.length());
+                }
+                JSONObject object = (JSONObject) new JSONTokener(inputString).nextValue();
+
+                String photoString = object.getString("photos");
+                if (null != photoString) {
+                    JSONObject jsonSubObject = (JSONObject) new JSONObject(photoString);
+                    String photoStream = jsonSubObject.getString("photo");
+                    if (photoStream != null) {
+//                        Log.i(TAG,"-------------");
+//                        Log.i(TAG, photoStream);
+                        // now we have json array object...
+                        arrayJSON = (JSONArray) new JSONTokener(photoStream).nextValue();
+                    }
+                }
+            }
+            catch(JSONException je){
+
+            }
+        }
+        return arrayJSON;
+    }
+
+    public ArrayList<GalleryItem> downloadGalleryItems(String  url) {
         ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
         String jsonString = null;
+
         try {
-            String url = Uri.parse(ENDPOINT).buildUpon()
+            jsonString = getUrl(url);
+
+
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+        JSONArray arrayPhotos = extractPhotos(jsonString);
+        try {
+            for (int i = 0; i < arrayPhotos.length(); ++i) {
+                GalleryItem item = new GalleryItem();
+                JSONObject jsonPhoto = arrayPhotos.getJSONObject(i);
+                item.setId(jsonPhoto.getString(JSON_ID));
+                item.setCaption(jsonPhoto.getString(JSON_CAPTION));
+                item.setUrl(jsonPhoto.getString(JSON_URL));
+                items.add(item);
+            }
+        }
+        catch(JSONException je ){
+            Log.i(TAG, "error parsing json flickr " + je);
+        }
+        return items;
+    }
+
+    public ArrayList<GalleryItem> fetchItems(){
+        ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+        String url = Uri.parse(ENDPOINT).buildUpon()
                     .appendQueryParameter("method", METHOD_GET_RECENT)
                     .appendQueryParameter("api_key", API_KEY)
                     .appendQueryParameter(PARAM_FORMAT, PARAM_JSON)
                     .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
                     .build().toString();
             Log.i(TAG, "requested URL is ------ " + url);
-            jsonString = getUrl(url);
+        return downloadGalleryItems(url);
 //            Log.i(TAG, "received items: " + jsonString);
-        }
-        catch (IOException ioe){
-            Log.e(TAG, "Failed to fetch items " + ioe);
-        }
-        if( jsonString != null ) {
-            try {
-                // need to remove jsonFlickrApi( from start
-                // need to remove ) from end for some crappy reason flickr put that in there
-                String start = "jsonFlickrApi(";
-                String end = ")";
-                if( jsonString.length() > start.length() + end.length()){
-                    jsonString = jsonString.substring(start.length(), jsonString.length()  - end.length());
-                }
-                JSONObject object = (JSONObject) new JSONTokener(jsonString).nextValue();
+    }
 
-                String photoString = object.getString("photos");
-                if( null != photoString ){
-                    JSONObject jsonSubObject = (JSONObject) new JSONObject(photoString);
-                    String photoStream = jsonSubObject.getString("photo");
-                    if( photoStream != null ){
-//                        Log.i(TAG,"-------------");
-//                        Log.i(TAG, photoStream);
-                        // now we have json array object...
-                        JSONArray arrayObject = (JSONArray) new JSONTokener(photoStream).nextValue();
-                        for( int i = 0; i < arrayObject.length(); ++i ){
-                            GalleryItem item = new GalleryItem();
-                            JSONObject jsonPhoto = arrayObject.getJSONObject(i);
-//                            Log.i(TAG, "Got object -" + jsonPhoto.toString());
-                            item.setId(jsonPhoto.getString(JSON_ID));
-                            item.setCaption(jsonPhoto.getString(JSON_CAPTION));
-                            item.setUrl(jsonPhoto.getString(JSON_URL));
-                            items.add(item);
-                        }
-                    }
-                }
-            } catch (JSONException je) {
-                Log.e(TAG, "json error" + je);
-            }
-        }
-        return items;
+    public ArrayList<GalleryItem> search(String query) {
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_SEARCH)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                .appendQueryParameter(PARAM_FORMAT, PARAM_JSON)
+                .appendQueryParameter(PARAM_TEXT, query)
+                .build().toString();
+        return downloadGalleryItems(url);
     }
 }
--- PhotoGallery/src/main/java/com/tackable/foobar/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/main/java/com/tackable/foobar/android/photogallery/PhotoGalleryFragment.java
@@ -1,17 +1,29 @@
 package com.tackable.foobar.android.photogallery;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,11 +38,53 @@ public class PhotoGalleryFragment extends Fragment {
 
     private static final String TAG = "PhotoGalleryFragment";
 
+    @TargetApi(11)
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // pull out the SearchView
+            MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+            SearchView searchView = (SearchView)searchItem.getActionView();
+
+            // get the data from our searchable.xml as a SearchableInfo
+            SearchManager searchManager = (SearchManager)getActivity()
+                    .getSystemService(Context.SEARCH_SERVICE);
+            ComponentName name = getActivity().getComponentName();
+            SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+
+            searchView.setSearchableInfo(searchInfo);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch( item.getItemId()){
+            case R.id.menu_item_clear:
+                PreferenceManager.getDefaultSharedPreferences(getActivity())
+                        .edit()
+                        .putString(FlickrFetchr.PREF_SEARCH_QUERY, null)
+                        .commit();
+                updateItems();
+                return true;
+            case R.id.menu_item_search:
+                getActivity().onSearchRequested();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void updateItems(){
+        new FetchItemsTask().execute();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        new FetchItemsTask().execute();
+        setHasOptionsMenu(true);
+        updateItems();
 
         mThumbnailThread = new ThumbnailDownloader<ImageView>(new android.os.Handler());
         mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
@@ -82,7 +136,17 @@ public class PhotoGalleryFragment extends Fragment {
     private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
         @Override
         protected ArrayList<GalleryItem> doInBackground(Void... params) {
-            return new FlickrFetchr().fetchItems();
+            Activity activity = getActivity();
+            if( activity == null ){
+                return new ArrayList<GalleryItem>();
+            }
+            String query = PreferenceManager.getDefaultSharedPreferences(activity).getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+            if( query != null ){
+                return new FlickrFetchr().search(query);
+            }
+            else {
+                return new FlickrFetchr().fetchItems();
+            }
         }
 
         @Override
